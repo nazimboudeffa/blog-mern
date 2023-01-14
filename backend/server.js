3343483b44a26dc0c67dab26ce9c2ad85f036075
nazimboudeffa <boudeffa@gmail.com>
2023-01-15 00:19:58 +0100

    Cleaned the repo

--- backend/server.js
+++ backend/server.js
@@ -28,9 +28,9 @@ const postRoutes = require("./routes/posts");
 app.use("/api/user", userRoutes);
 app.use("/api/posts", postRoutes);
 
-const PORT = process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 
 //server setup
-app.listen(PORT, () => {
-  console.log(`Server running on PORT:${process.env.PORT}`);
+app.listen(port, () => {
+  console.log(`Server running on port:${port}`);
 });
